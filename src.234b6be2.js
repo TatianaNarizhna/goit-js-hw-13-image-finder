f0265fa322739cc7d4006c7de38df54c65fbf986
TatianaNarizhna <narizhna.tania@gmail.com>
2021-06-01 18:20:58 +0300

    Updates

--- src.234b6be2.js
+++ src.234b6be2.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./code.js/main");
 },{"./sass/main.scss":"clu1","./code.js/main":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-13-image-finder/src.96153dcf.js.map
+//# sourceMappingURL=/goit-js-hw-13-image-finder/src.234b6be2.js.map
